67fd57caaea18779331d5653518bb5150736a166
meckblvck <meckydrix@gmail.com>
2017-12-20 10:45:03 +0100

    Add project files.

---
+++ Odarms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Odarms.Startup))]
+namespace Odarms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

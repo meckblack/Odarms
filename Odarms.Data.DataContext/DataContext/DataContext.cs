7427e02a3661edcdc884f1e79a820cadb179ae85
meckblvck <meckydrix@gmail.com>
2017-12-20 12:29:38 +0100

    Added a dataContext to Odarms.Data.DataContext

---
+++ Odarms.Data.DataContext/DataContext/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Odarms.Data.DataContext.DataContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext ()
+            : base ("name = Odarms")
+        {
+
+        }
+
+        #region AppUserContext
+
+        #endregion 
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}

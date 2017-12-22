f7c903ca51aa3e57f337d21a26cfa8ecda642a51
meckblvck <meckydrix@gmail.com>
2017-12-22 23:40:16 +0100

    Added Employee Model

---
+++ Odarms.Data.Objects/Entities.Employee/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odarms.Data.Objects.Entities.Employee
+{
+    public class Employee
+    {
+        #region Foreign Keys
+        public long RestaurantId { get; set; }
+        [ForeignKey("RestaurantId")]
+        public int MyProperty { get; set; }
+        #endregion
+    }
+}

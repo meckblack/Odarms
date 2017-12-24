2fdddb80c8a6eb1bfb90c53b2b2fb794e11d99f8
meckblvck <meckydrix@gmail.com>
2017-12-24 22:56:41 +0100

    I added the Department model

---
+++ Odarms.Data.Objects/Entities.SystemManagement/Department.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odarms.Data.Objects.Entities.SystemManagement
+{
+    public class Department
+    {
+        #region Tables
+
+        public long DepartmentId { get; set; }
+
+        [Required(ErrorMessage="Name field is requried")]
+        public string Name { get; set; }
+
+        [DisplayName("Restaraunt Name")]
+        public long RestaurantId { get; set; }
+
+        #endregion
+
+        #region Foreign Keys
+
+        [ForeignKey("RestaurantId")]
+        public virtual Restaurant Restaurant { get; set; }
+
+        public long? EmployeeId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public virtual Employee.Employee Employee { get; set; }
+
+        #endregion
+
+
+        public IEnumerable<Employee.Employee> Employees { get; set; }
+
+    }
+}

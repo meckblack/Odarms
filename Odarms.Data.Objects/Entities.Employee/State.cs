d60471b9ec26fb2a7439379b277d641d0cc142f9
meckblvck <meckydrix@gmail.com>
2017-12-25 00:05:03 +0100

    Added State model

---
+++ Odarms.Data.Objects/Entities.Employee/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odarms.Data.Objects.Entities.Employee
+{
+    public class State
+    {
+        [Key]
+        public int StateId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        //public IEnumerable<Lga> Lgas { get; set; }
+    }
+}

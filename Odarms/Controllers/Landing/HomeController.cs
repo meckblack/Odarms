57166f8cbc75813cdc564fb2f3589d4b03578c4e
meckblvck <meckydrix@gmail.com>
2017-12-26 00:56:34 +0100

    Changed HomeController namespace, Added new LandingLayout

--- Odarms/Controllers/Landing/HomeController.cs
+++ Odarms/Controllers/Landing/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace Odarms.Controllers
+namespace Odarms.Controllers.Landing
 {
     public class HomeController : Controller
     {

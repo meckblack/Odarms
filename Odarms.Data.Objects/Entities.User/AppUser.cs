03e20f1fa3709764223e2e93b2c0ba24cac8ba31
meckblvck <meckydrix@gmail.com>
2017-12-25 03:32:20 +0100

    Created AppUser model

---
+++ Odarms.Data.Objects/Entities.User/AppUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odarms.Data.Objects.Entities.User
+{
+    public class AppUser
+    {
+        public long AppUserId { get; set; }
+
+        [DisplayName("First Name"), Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name"), Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [DisplayName("Middle Name")]
+        public string MiddleName { get; set; }
+
+        [EmailAddress, Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [DisplayName("Mobile Number"), Required(ErrorMessage = "Moblie number is required")]
+        public string Mobile { get; set; }
+
+        [DataType(DataType.Password), Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Required(ErrorMessage = "Comfirm password is required")]
+        [Compare("Password"), DisplayName("Comfrim Password")]
+        public string ComfirmPassword { get; set; }
+
+    }
+}

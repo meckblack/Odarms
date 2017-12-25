a8cdd3e7761ce23efd982061f912d03805399de3
meckblvck <meckydrix@gmail.com>
2017-12-25 02:46:19 +0100

    Added BloodGroup enum

---
+++ Odarms.Data.Service/Enums/BloodGroup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odarms.Data.Service.Enums
+{
+    public enum BloodGroup
+    {
+        [Display(Name = "O+")]
+        Opositive,
+        [Display(Name = "O-")]
+        Onegative,
+        [Display(Name = "A+")]
+        Apositive,
+        [Display(Name = "A-")]
+        Anegative,
+        [Display(Name = "B+")]
+        Bpositive,
+        [Display(Name = "B-")]
+        Bnegative,
+        [Display(Name = "AB+")]
+        ABpositive,
+        [Display(Name = "AB-")]
+        ABnegative
+    }
+}

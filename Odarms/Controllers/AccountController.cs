23c01b576a5df8b447cc6c063eafa655b43ed4ae
meckblvck <meckydrix@gmail.com>
2017-12-27 23:39:57 +0100

    Few changes

--- Odarms/Controllers/AccountController.cs
+++ Odarms/Controllers/AccountController.cs
@@ -204,12 +204,13 @@ namespace Odarms.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Register(RegisterViewModel model, Restaurant restaurant, FormCollection collectedValues)
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                
+                var user = new ApplicationUser { UserName = restaurant.ContactEmail, Email = restaurant.ContactEmail };
+                var result = await UserManager.CreateAsync(user, restaurant.ContactEmail);
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);

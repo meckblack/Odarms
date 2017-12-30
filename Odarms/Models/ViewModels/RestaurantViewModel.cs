b887c6211639b4beae2645fb4264ffa43d88343a
meckblvck <meckydrix@gmail.com>
2017-12-30 22:40:13 +0100

    Added RestaurantViewModel

---
+++ Odarms/Models/ViewModels/RestaurantViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Odarms.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        //[Required(ErrorMessage = "Name field is required")]
+        //[DisplayName("Company Name")]
+        //public string Name { get; set; }
+
+        //public string Motto { get; set; }
+
+        //public string Logo { get; set; }
+
+        //[Required]
+        //[EmailAddress]
+        //[Display(Name = "Email")]
+        //public string Email { get; set; }
+
+        //[Required]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Password")]
+        //public string Password { get; set; }
+
+        //[DataType(DataType.Password)]
+        //[Display(Name = "Confirm password")]
+        //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        //public string ConfirmPassword { get; set; }
+    }
+}

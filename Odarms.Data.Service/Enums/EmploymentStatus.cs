25e96c1d7fcd0da977c0f9e16b2e0b9b6ed7cde7
meckblvck <meckydrix@gmail.com>
2018-01-10 13:45:53 +0100

    changes

--- Odarms.Data.Service/Enums/EmploymentStatus.cs
+++ Odarms.Data.Service/Enums/EmploymentStatus.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Odarms.Data.Service.Enums
 {
-    public class EmploymentStatus
+    public enum EmploymentStatus
     {
         Active,
         Retired,

4c64bb62f36c795252cb8e2d9a9922d0a197ce39
meckblvck <meckydrix@gmail.com>
2018-03-11 14:55:33 +0100

    Major changes

--- Odarms/Controllers/SystemManagement/RestaurantsController.cs
+++ Odarms/Controllers/SystemManagement/RestaurantsController.cs
@@ -14,7 +14,7 @@ using System.Net.Mail;
 
 namespace Odarms.Controllers.SystemManagement
 {
-    [Authorize]
+    
     public class RestaurantsController : Controller
     {
         private readonly DataContext _db = new DataContext();
